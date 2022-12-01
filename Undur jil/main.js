d62d550585f322fbee448d78d4dfd41b09a02a7c
TealOyuErdene <118173533+TealOyuErdene@users.noreply.github.com>
2022-12-02 03:00:43 +0800

    Add files via upload

---
+++ Undur jil/main.js
@@ -0,0 +1,10 @@
+/*Undur jil*/
+const year = prompt('Jilee oruulna uu')
+
+if(year % 4 == 0){
+     console.log('Mun')
+}else if (year % 100 == 0){
+     console.log('Bish')
+}else(
+    console.log('Bish')
+)
